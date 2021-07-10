7549aaa664f2d800f88437d927c588a8229a50ad
Luis Alberto Cevallos Cavero <lcevallo@gmail.com>
2021-07-10 07:39:35 -0500

    Actualizacion
    Con el metodo que faltaba

--- controllers/ClienteController.js
+++ controllers/ClienteController.js
@@ -1,36 +1,40 @@
 'use strict'
 
 var Cliente = require('../models/cliente');
+var bcrypt = require('bcrypt-nodejs');
 
 const registro_cliente = async function(req, res){
-    //
-    res.status(200).send({
-        message: 'Hola Mundo'
-    });
-}
-
-const login_cliente = async function(req, res){
+    
     var data = req.body;
-    var cliente_arr = [];
+    var clientes_arr = [];
 
-    cliente_arr = await Cliente.find({email: data.email});
+    clientes_arr = await Cliente.find({email: data.email});
 
-    if(cliente_arr.length == 0) {
-        res.status(200).send({message: 'No se encontro el correo', data: undefined});
-    } else {
-        let user = cliente_arr[0];
+    if(clientes_arr.length == 0) {
+        if(data.password) {
+            bcrypt.hash(data.passwo, null, async function(err, hash) {
+                if(hash){
+                    data.password = hash;
+                    var reg = await Cliente.create(data);
+                    res.status(200).send({data:reg});
+                } else {
+                    res.status(200).send({message: 'ErrorServer', data: undefined});
+                }
+            } )
         
-        if(user.password == data.password) {
-            res.status(200).send({data:user})
-        } else{
-            res.status(200).send({message: 'La contraseña no coincide', data: undefined});
+        }else {
+            res.status(200).send({message: 'No hay una contraseña', data: undefined});
         }
-        
-
+    } else{
+        res.status(200).send({message: 'El correo ya existe en la base de datos', data: undefined})
     }
+} 
 
-    res.status(200).send({data: data});
 
+const login_cliente= async function(req, res){
+    var data = req.body;
+
+    res.status(200).send({data:data});
 }
 
 module.exports = {
